11700724efb3ec8a257349238520b0957a060fea
Vamsi Krishna B <vamsi@fastmail.com>
2016-09-11 11:04:24 +0530

    tools

--- src/AppBundle/Controller/InfoGatheringController.php
+++ src/AppBundle/Controller/InfoGatheringController.php
@@ -114,6 +114,57 @@ class InfoGatheringController extends Controller
 
     }
 
+    /**
+     * @param Request $request
+     * @Route("/tool")
+     */
+    public function ToolAction(Request $request)
+    {
+        $data = $request->request->all();
+
+        $data = $request->request->all();
+        $host = $data['text'];
+        $command = ltrim($data['command'],'/');
+
+
+
+
+        switch ($command) {
+            case 'dnslookup' :
+                $uri = 'dnslookup';
+                break;
+            case 'reversedns':
+                $uri = 'reversedns';
+                break;
+            case 'hostsearch' :
+                $uri = 'hostsearch';
+                break;
+            case  'findshareddns':
+                $uri = 'findshareddns';
+                break;
+            case 'zonetransfer':
+                $uri = 'zonetransfer';
+                break;
+            case 'whois':
+                $uri = 'whois';
+                break;
+            default;
+                $uri = null;
+                break;
+        }
+
+        $url_to_check = "http://api.hackertarget.com/$uri/?q=".$host;
+        $user_agent = "Slack/1.0";
+        $ch = curl_init($url_to_check);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+        $resp = curl_exec($ch);
+        curl_close($ch);
+
+        return new Response($resp);
+
+    }
+
 
 
 
